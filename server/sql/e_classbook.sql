8ece1eafd63c6d9f0237f29e667288e3db06f320
MarkusWaller <waller.markus@gmail.com>
2015-04-22 20:24:26 +0200

    SQL Dateien
    Struktur und Beispiel Daten

---
+++ server/sql/e_classbook.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 22. Apr 2015 um 20:12
+-- Server Version: 5.6.21
+-- PHP-Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `e_classbook`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `book`
+--
+
+CREATE TABLE IF NOT EXISTS `book` (
+`book_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `subject` varchar(32) NOT NULL,
+  `teacher` int(11) NOT NULL,
+  `info` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `class`
+--
+
+CREATE TABLE IF NOT EXISTS `class` (
+  `name` varchar(16) NOT NULL,
+  `h_teacher` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `student`
+--
+
+CREATE TABLE IF NOT EXISTS `student` (
+`student_id` int(11) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `class` varchar(16) NOT NULL,
+  `birth_date` date NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `teacher`
+--
+
+CREATE TABLE IF NOT EXISTS `teacher` (
+`teacher_id` int(11) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(32) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `book`
+--
+ALTER TABLE `book`
+ ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indizes für die Tabelle `class`
+--
+ALTER TABLE `class`
+ ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indizes für die Tabelle `student`
+--
+ALTER TABLE `student`
+ ADD PRIMARY KEY (`student_id`), ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indizes für die Tabelle `teacher`
+--
+ALTER TABLE `teacher`
+ ADD PRIMARY KEY (`teacher_id`), ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `book`
+--
+ALTER TABLE `book`
+MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT für Tabelle `student`
+--
+ALTER TABLE `student`
+MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `teacher`
+--
+ALTER TABLE `teacher`
+MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ server/sql/e_classbook_data.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 22. Apr 2015 um 20:18
+-- Server Version: 5.6.21
+-- PHP-Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `e_classbook`
+--
+
+--
+-- Daten für Tabelle `book`
+--
+
+INSERT INTO `book` (`book_id`, `date`, `subject`, `teacher`, `info`) VALUES
+(1, '2015-04-22', 'IT', 1, 'Klassenarbeit geschrieben');
+
+--
+-- Daten für Tabelle `class`
+--
+
+INSERT INTO `class` (`name`, `h_teacher`) VALUES
+('TGQ1c', 1);
+
+--
+-- Daten für Tabelle `student`
+--
+
+INSERT INTO `student` (`student_id`, `first_name`, `last_name`, `class`, `birth_date`, `email`, `password`) VALUES
+(1, 'Markus', 'Waller', 'TGQ1c', '1996-12-07', 'waller.markus@gmail.com', 'MeinPasswort');
+
+--
+-- Daten für Tabelle `teacher`
+--
+
+INSERT INTO `teacher` (`teacher_id`, `last_name`, `first_name`, `email`, `password`) VALUES
+(1, 'Mustermann', 'Max', 'max@mustermann.de', 'Musterpasswort');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
